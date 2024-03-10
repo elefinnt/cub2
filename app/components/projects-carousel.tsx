eaf4689b26638a292dc710a75eaab13b18ef5ff5
Finn Hackshaw <hackshawf5@gmail.com>
2024-03-11 12:49:44 +1300

    chore: cleanup

--- app/components/projects-carousel.tsx
+++ app/components/projects-carousel.tsx
@@ -5,7 +5,7 @@ const ProjectsCarousel = () => {
     <div className="content-container py-20 bg-[#f1f1f1]">
       <div>
         <h1 className="mb-8 text-[62px] font-semibold -tracking-[1px]">
-          AI consulting projects we've delivered
+          AI consulting projects we&apos;'ve delivered
         </h1>
         <div className="overflow-visible">
           <CarouselSize />
