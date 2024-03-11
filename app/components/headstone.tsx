b1de9ee4fff787c43a751210f3a7a744915a3505
Finn Hackshaw <hackshawf5@gmail.com>
2024-03-11 14:11:12 +1300

    fix: layout and structures

--- app/components/headstone.tsx
+++ app/components/headstone.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
 const Headstone = () => {
-  return <div className="headstone-container"></div>;
+  return (
+    <div className=" bg-black">
+      <div className="headstone-wrap">
+        <div className="headstone-first">
+          <div className="headstone-logo">
+            {/* <Image src="/logo.png" fill alt="logo" /> */}
+          </div>
+          <div className="headstone-title"></div>
+        </div>
+        <div className="headstone-second"></div>
+        <div className="headstone-third"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Headstone;
