e35fe23477f6c96e840f1505e686cfb7ba6d297d
Finn Hackshaw <hackshawf5@gmail.com>
2024-03-11 16:25:19 +1300

    feat: reviews component

--- app/components/reference-presentation.tsx
+++ app/components/reference-presentation.tsx
@@ -1,5 +1,39 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
+
 const ReferencePresentation = () => {
-  return <div className="content-container-sm">Reference Presentation</div>;
+  return (
+    <div className="bg-black">
+      <div className="content-container-sm py-20">
+        <div className="reviews-content text-white flex">
+          <div className="reviews-title">
+            CUB&apos;s team are experts in their trade. The software development
+            they provided was critical to advancing the project to the next
+            level.
+          </div>
+          <div className="reviews-author">
+            <div className="reviews-photo">
+              <Image
+                src="/profile-circle.png"
+                alt="Profile picture"
+                width={88}
+                height={88}
+              />
+            </div>
+            <div className="reviews-author-info">
+              <div>Chris Dunlop</div>
+              <div>CTO</div>
+              <div>Another Company</div>
+            </div>
+          </div>
+          <div className="reviews-nav">
+            <ChevronLeft />
+            <ChevronRight />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReferencePresentation;
